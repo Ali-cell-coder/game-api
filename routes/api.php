94a029671b540847a66a0d8ebe03b0a4c9036f0a
Ali Çağan <ozgucualicagan@gmail.com>
2026-09-11 12:20:43 +0300

    Add game-related routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/games/start', [GameController::class, 'start']);
+
+Route::post('/games/{id}/end', [GameController::class, 'end']);
+
+Route::get('/games/daily-top-10', [GameController::class, 'dailyTop10']);
