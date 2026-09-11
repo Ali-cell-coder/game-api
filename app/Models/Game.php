febc40996bcbc2ff05c0708d0cef9f83603399cd
Ali Çağan <ozgucualicagan@gmail.com>
2026-09-11 12:13:59 +0300

    Add user relationship and fillable fields to Game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Game extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'score',
+        'started_at',
+        'ended_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);//BİR OYUN 1 OYUNCUYA AİT
+        
+    }
 }
