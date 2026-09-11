cd80e1fbb2a085c9387f95b60b65b1d65d903780
Ali Çağan <ozgucualicagan@gmail.com>
2026-09-11 12:18:41 +0300

    Create games table with user_id and score fields
    Added foreign key for user_id and score, started_at, and ended_at timestamps.

--- database/migrations/2026_09_10_195226_create_games_table.php
+++ database/migrations/2026_09_10_195226_create_games_table.php
@@ -1,27 +1,26 @@
 <?php
 
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
+
     /**
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('games', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    
+     Schema::create('games', function (Blueprint $table) {
+    $table->id();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('games');
-    }
-};
+    $table->foreignId('user_id')
+        ->constrained()
+        ->onDelete('cascade');
+
+    $table->integer('score');
+    $table->timestamp('started_at');
+    $table->timestamp('ended_at')
+});
